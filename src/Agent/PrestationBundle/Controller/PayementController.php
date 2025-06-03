86c7db0f7217dadc288a49c23f89b0f918733c9a
ZAHFOUF YOUNES <youneszahfouf@gmail.com>
2025-06-03 13:10:19 +0100

    correction

--- src/Agent/PrestationBundle/Controller/PayementController.php
+++ src/Agent/PrestationBundle/Controller/PayementController.php
@@ -8,11 +8,11 @@ use AlAya\Common\Controller\BaseController;
 use AlAya\Common\Entity\Payement;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Component\Routing\Annotation\Route;
-#[Access()]
 class PayementController extends BaseController
 {
     #[Route('/payements', name: 'payement_index')]
     #[Template("@AgentPrestationBundle/Payement/index.html.twig")]
+    #[Access()]
     public function index()
     {
         $payements = $this->repo(Payement::class)->findAll();
